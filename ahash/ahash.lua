d938655f754f91b435407bd67c54474e71adffc4
monkoose <pythonproof@gmail.com>
2024-09-27 11:15:31 +0300

    fix: ahash does more work then required
    Even though ahash module caches hash() values, it is on every key lookup still checking for its existence executing `__index` function. Removing intermediate table fixes this.

--- ahash/ahash.lua
+++ ahash/ahash.lua
@@ -1,14 +1,6 @@
-local M = {}
-
-local hashes = {}
-local hash_metatable = {
-	__index = function(hashes_table, hash_key)
-		if hashes[hash_key] == nil then
-			rawset(hashes, hash_key, hash(hash_key))
-		end
-		return hashes[hash_key]
-	end
-}
-setmetatable(M, hash_metatable)
-
-return M
+return setmetatable({}, {
+    __index = function(t, k)
+        rawset(t, k, hash(k))
+        return t[k]
+    end,
+})
